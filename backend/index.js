56066bad8f878d80528fce1e91c5658996aa0e57
Hemant soni <sonihemant4741@gmail.com>
2023-03-16 14:12:43 +0530

    Intialise frontend and Backend

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from 'body-parser';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+  });
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
